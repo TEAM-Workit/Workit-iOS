306a4b095e36a3d15573e44e67f551f4fe742879
dev-madilyn <lollin0@naver.com>
2022-11-21 22:23:08 +0900

    Add: UIViewController+ 화면 터치 시 작성 종료하는 메서드 #8

---
+++ Projects/DesignSystem/Sources/UIViewController+.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewController+.swift
+//  DesignSystem
+//
+//  Created by madilyn on 2022/11/19.
+//  Copyright © 2022 com.workit. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    /// 화면 터치시 작성 종료하는 메서드
+    open override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+}
