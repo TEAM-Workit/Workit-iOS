e402a5f414aaba236181372ec74adaac52448861
dev-madilyn <lollin0@naver.com>
2022-11-21 22:22:35 +0900

    Refactor: WKInputTextField 코드 정리 #8

--- Projects/DesignSystem/Sources/UIComponents/WKInputTextField.swift
+++ Projects/DesignSystem/Sources/UIComponents/WKInputTextField.swift
@@ -11,10 +11,10 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-class WKInputTextField: UITextField {
+public class WKInputTextField: UITextField {
     
     // MARK: Components
-    let clearButton: UIButton = UIButton()
+    private let clearButton: UIButton = UIButton()
     
     // MARK: Properties
     private let disposeBag: DisposeBag = DisposeBag()
@@ -22,59 +22,78 @@ class WKInputTextField: UITextField {
     // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setDefaultLayout()
         self.setDefaultStyle()
-        self.setTextFieldClearBtn(textField: self, clearBtn: self.clearButton)
+        self.setTextFieldClearBtn(textField: self, targetClearButton: self.clearButton)
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.setDefaultStyle()
-        self.setTextFieldClearBtn(textField: self, clearBtn: self.clearButton)
     }
     
     // MARK: Functions
     /// textField-btn 에 clear 기능 세팅하는 함수
-    private func setTextFieldClearBtn(textField: UITextField, clearBtn: UIButton) {
+    private func setTextFieldClearBtn(textField: UITextField, targetClearButton: UIButton) {
         textField.rx.text
             .orEmpty
             .distinctUntilChanged()
             .subscribe(onNext: { changedText in
-                clearBtn.isHidden = changedText != "" ? false : true
-            })
-            .disposed(by: disposeBag)
+                targetClearButton.isHidden = changedText != "" ? false : true
+            }).disposed(by: disposeBag)
         
         /// Clear 버튼 액션
-        clearButton.rx.tap
+        targetClearButton.rx.tap
             .bind {
                 textField.text = ""
-                clearBtn.isHidden = true
-            }
-            .disposed(by: disposeBag)
+                targetClearButton.isHidden = true
+            }.disposed(by: disposeBag)
     }
 }
 
 // MARK: - UI
 extension WKInputTextField {
+    private func setTextFieldActiveStyle() {
+        self.rx.controlEvent(.allEditingEvents)
+            .subscribe(onNext: { _ in
+                self.layer.borderColor = UIColor.purple.cgColor
+                self.layer.borderWidth = 2
+            }).disposed(by: disposeBag)
+        
+        self.rx.controlEvent(.editingDidEnd)
+            .subscribe(onNext: { _ in
+                self.layer.borderColor = UIColor.lightGray.cgColor
+                self.layer.borderWidth = 1
+            }).disposed(by: disposeBag)
+    }
+    
     private func setClearButtonUI() {
-        self.clearButton.setImage(UIImage(systemName: "x.circle.fill"), for: .normal)
+        self.clearButton.setImage(UIImage(systemName: "x.circle.fill")?.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    private func setDefaultLayout() {
+        self.addSubviews([clearButton])
+        
+        self.clearButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.top.bottom.equalToSuperview().inset(13)
+            make.right.equalToSuperview().inset(10)
+            make.width.equalTo(clearButton.snp.height)
+        }
     }
     
     private func setDefaultStyle() {
-        self.addSubviews([clearButton])
-        
-        self.clearButton.snp.makeConstraints {
-            $0.centerY.equalToSuperview()
-            $0.top.bottom.equalToSuperview().inset(13)
-            $0.right.equalToSuperview().inset(10)
-            $0.width.equalTo(clearButton.snp.height)
-        }
-        
-        self.backgroundColor = .lightGray
-        self.layer.cornerRadius = 11
-        self.addLeftPadding(17)
-        self.addRightPadding(34)
-//        self.font = .mumentH4M16
-        self.attributedPlaceholder = NSAttributedString(string: self.placeholder ?? "입력", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
+        // TODO: font, color 변경
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 5
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1
+        self.addLeftPadding(12)
+        self.addRightPadding(44)
+        self.attributedPlaceholder = NSAttributedString(string: self.placeholder ?? "placeholder", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray])
         self.textColor = .black
+        
+        self.setTextFieldActiveStyle()
+        self.setClearButtonUI()
+        self.returnKeyType = .done
     }
 }
