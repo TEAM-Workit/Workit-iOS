dbfea1fba163e6b214b7a03c764bb8b8a45e847a
yejiyun <20172062@sungshin.ac.kr>
2022-11-29 17:06:35 +0900

    Feat: add WKNavigationBar used on the modal screen (#13)

---
+++ Projects/DesignSystem/Sources/UIComponents/Navigation/WKNavigationBar.swift
@@ -0,0 +1,32 @@
+//
+//  WKNavigationBar.swift
+//  DesignSystem
+//
+//  Created by 윤예지 on 2022/11/29.
+//  Copyright © 2022 com.workit. All rights reserved.
+//
+
+import UIKit.UINavigationBar
+
+/// NavigationController의 NavigationBar를 사용할 수 없을 때 (Modal Present 등)
+/// 단독으로 사용할 수 있는 NavigationBar입니다.
+public class WKNavigationBar: UINavigationBar {
+
+    public init() {
+        super.init(frame: .zero)
+        
+        setStyle()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setStyle() {
+        self.pushItem(UINavigationItem(), animated: true)
+        self.shadowImage = UIImage()
+        self.isTranslucent = true
+        self.barTintColor = UIColor.wkWhite
+    }
+ 
+}
