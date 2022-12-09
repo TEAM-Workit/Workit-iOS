5285bffa8e4de15ca20bd8f47d3aa6a6c8ac1d59
dev-madilyn <lollin0@naver.com>
2022-12-10 02:18:54 +0900

    Chore: WriteViewController 레이아웃 메서드 구현 (#49)

--- Projects/App/Sources/Screens/Write/WriteViewController.swift
+++ Projects/App/Sources/Screens/Write/WriteViewController.swift
@@ -106,4 +106,124 @@ final class WriteViewController: BaseViewController {
         self.setWorkDescriptionTextView()
     }
     
+// MARK: - UI
+
+extension WriteViewController {
+    override func setLayout() {
+        self.setSubviews()
+        self.setBackgroundLayout()
+        self.setDateLayout()
+        self.setProjectLayout()
+        self.setWorkLayout()
+        self.setAbilityLayout()
+        self.setWorkDescriptionLayout()
+    }
+    
+    private func setSubviews() {
+        self.view.addSubviews([navigationBar, scrollView])
+        self.contentView.addSubviews([
+            dateLabel, dateButton,
+            projectLabel, projectButton,
+            workLabel, workTextField,
+            abilityLabel, abilityCollectionView, abilityAddButton,
+            workDescriptionLabel, workDescriptionTextView, workDescriptionCountLabel
+        ])
+    }
+    
+    private func setBackgroundLayout() {
+        self.navigationBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+        }
+        
+        self.scrollView.snp.makeConstraints { make in
+            make.top.equalTo(self.navigationBar.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        self.scrollView.addSubview(contentView)
+        
+        self.contentView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.centerX.top.bottom.equalToSuperview()
+        }
+    }
+    
+    private func setDateLayout() {
+        self.dateLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().inset(20)
+        }
+        
+        self.dateButton.snp.makeConstraints { make in
+            make.top.equalTo(self.dateLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
+    private func setProjectLayout() {
+        self.projectLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.dateButton.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        self.projectButton.snp.makeConstraints { make in
+            make.top.equalTo(self.projectLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
+    private func setWorkLayout() {
+        self.workLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.projectButton.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        self.workTextField.snp.makeConstraints { make in
+            make.top.equalTo(self.workLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(44)
+        }
+    }
+    
+    private func setAbilityLayout() {
+        self.abilityLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.workTextField.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        self.abilityCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(self.abilityLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(0)
+        }
+        
+        self.abilityAddButton.snp.makeConstraints { make in
+            make.top.equalTo(self.abilityCollectionView.snp.bottom).offset(8)
+            make.leading.equalToSuperview().inset(20)
+            make.width.equalTo(111)
+            make.height.equalTo(29)
+        }
+    }
+    
+    private func setWorkDescriptionLayout() {
+        self.workDescriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.abilityAddButton.snp.bottom).offset(32)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        self.workDescriptionTextView.snp.makeConstraints { make in
+            make.top.equalTo(self.workDescriptionLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(306)
+        }
+        
+        self.workDescriptionCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.workDescriptionTextView.snp.bottom).offset(8)
+            make.trailing.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(28)
+            make.height.equalTo(15)
+        }
+    }
+    
 }
