638bf02df96b388d7d2066e8c767425af04cb4d2
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-12-21 17:47:02 +0900

    Feat: add Home Navigation Bar (#48)

--- Projects/App/Sources/Screens/Home/HomeViewController.swift
+++ Projects/App/Sources/Screens/Home/HomeViewController.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2022 com.workit. All rights reserved.
 //
 
+import DesignSystem
 import UIKit
 
-class HomeViewController: UIViewController {
-    
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setNavigationBar()
+        setUI()
+    }
+
+    private func setUI() {
+        view.backgroundColor = .wkMainPurple
+    }
+
+    private func setNavigationBar() {
+        self.navigationController?.setNavigationBarApperance(backgroundColor: .clear, tintColor: .wkWhite)
+        let button = WKNavigationButton(image: Image.wkMenu)
+        let barbuttonitem = UIBarButtonItem(customView: button)
+        self.navigationItem.rightBarButtonItem = barbuttonitem
+    }
+
 }
