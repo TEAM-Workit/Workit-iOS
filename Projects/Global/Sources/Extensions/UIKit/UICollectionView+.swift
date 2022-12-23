1e81aa70add0971edc1673bb4f95bfb65ddc746a
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2022-12-24 02:22:49 +0900

    fix: change collectionview extension (#48)

--- Projects/Global/Sources/Extensions/UIKit/UICollectionView+.swift
+++ Projects/Global/Sources/Extensions/UIKit/UICollectionView+.swift
@@ -8,49 +8,59 @@
 
 import UIKit.UICollectionView
 
-protocol NibLoadable: AnyObject {
-    static var nibName: String { get }
-}
+public protocol ReusableView: AnyObject {}
 
-protocol ReusableCell {
-    static var reuseIdentifier: String { get }
-}
-
-extension ReusableCell {
-    public static var reuseIdentifier: String {
+public extension ReusableView where Self: UIView {
+    static var reuseIdentifier: String {
         return String(describing: self)
     }
 }
 
-extension NibLoadable where Self: UIView {
-    internal static var nibName: String {
-        return NSStringFromClass(self).components(separatedBy: ".").last!
-    }
-}
+extension UICollectionReusableView: ReusableView {}
 
-extension UICollectionView {
-    public func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
-        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier, for: indexPath) as? T else {
-            fatalError("Unable Dequeue Reusable")
+public extension UICollectionView {
+    
+    func register<T>(
+        cell: T.Type,
+        forCellWithReuseIdentifier reuseIdentifier: String = T.reuseIdentifier
+    ) where T: UICollectionViewCell {
+        self.register(cell, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+    
+    func registerHeader<T: UICollectionReusableView>(_ viewType: T.Type) {
+        self.register(viewType.self,
+                      forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                      withReuseIdentifier: String(describing: viewType.self))
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.reuseIdentifier,
+                                             for: indexPath) as? T
+        else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
         return cell
     }
     
-    public func dequeueReusableView<T: UICollectionReusableView>(_: T.Type, indexPath: IndexPath, kind: String) -> T {
-        guard let view = dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: T.className, for: indexPath as IndexPath) as? T else {
-            fatalError("Unable Dequeue Reusable")
+    func dequeueHeaderView<T: UICollectionReusableView>(for indexPath: IndexPath) -> T {
+        guard let view = dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: T.reuseIdentifier,
+            for: indexPath
+        ) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
         }
         return view
     }
     
-    public func register<T: UICollectionViewCell>(_: T.Type) {
-        register(T.self, forCellWithReuseIdentifier: T.reuseIdentifier)
-    }
-    
-    public func registerReusableView<T: UICollectionReusableView>(_: T.Type, kind: String) {
-        register(T.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: T.className)
+    func dequeueFooterView<T: UICollectionReusableView>(for indexPath: IndexPath) -> T {
+        guard let view = dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionFooter,
+            withReuseIdentifier: T.reuseIdentifier,
+            for: indexPath
+        ) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.reuseIdentifier)")
+        }
+        return view
     }
 }
-
-extension UICollectionViewCell: NibLoadable { }
-extension UICollectionViewCell: ReusableCell { }
