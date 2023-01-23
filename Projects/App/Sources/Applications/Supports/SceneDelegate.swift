382bb82684025ec1d6f7e0bf3ee3f57377b80cba
yejiyun <20172062@sungshin.ac.kr>
2023-01-23 13:38:46 +0900

    feat: add Tabbar Resource (#46)

--- Projects/App/Sources/Applications/Supports/SceneDelegate.swift
+++ Projects/App/Sources/Applications/Supports/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let viewController = WKNavigationConroller(rootViewController: HomeViewController())
+        let viewController = WKTabbarController()
         viewController.view.backgroundColor = UIColor.wkWhite
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
