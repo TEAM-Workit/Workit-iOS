66a6b6067fc0965dbec941cd64248f2807197af7
yunyezl <yun72027@gmail.com>
2023-01-24 23:27:24 +0900

    feat: add settingView UI

---
+++ Projects/App/Sources/Screens/Setting/SettingViewController.swift
@@ -0,0 +1,129 @@
+//
+//  SettingViewController.swift
+//  App
+//
+//  Created by yjiyuni-MN on 2023/01/24.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+import DesignSystem
+import Global
+import UIKit
+
+import SnapKit
+
+class SettingViewController: UIViewController {
+    
+    enum Setting: CaseIterable {
+        case project
+        case service
+        case inquiry
+        case policy
+        case logout
+        
+        var title: String {
+            switch self {
+            case .project: return "프로젝트 관리"
+            case .service: return "서비스 소개"
+            case .inquiry: return "문의하기"
+            case .policy: return "정책 및 약관"
+            case .logout: return "로그아웃"
+            }
+        }
+    }
+    
+    enum Text {
+        static let withdraw = "탈퇴하기"
+    }
+    
+    // MARK: - UIComponents
+    
+    private let profileView = UserProfileView()
+    
+    private let withdrawButton: UIButton = {
+        let label = UIButton()
+        label.setTitle(Text.withdraw, for: .normal)
+        label.titleLabel?.font = .b1M
+        label.setTitleColor(.wkBlack45, for: .normal)
+        return label
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(SettingTableViewCell.self)
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setTableView()
+        self.setLayout()
+        self.setNavigationBar()
+    }
+    
+    // MARK: - Methods
+    
+    private func setTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.isScrollEnabled = false
+    }
+
+    private func setLayout() {
+        self.view.addSubviews([profileView, tableView, withdrawButton])
+
+        self.profileView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(120)
+        }
+        
+        self.tableView.snp.makeConstraints { make in
+            make.top.equalTo(profileView.snp.bottom)
+            make.leading.bottom.trailing.equalToSuperview()
+        }
+        
+        self.withdrawButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-45)
+        }
+    }
+
+    private func setNavigationBar() {
+        self.navigationController?.setNavigationBarApperance(
+            backgroundColor: .wkMainPurple,
+            tintColor: .wkWhite)
+        self.navigationItem.title = "설정"
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension SettingViewController: UITableViewDelegate {
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension SettingViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Setting.allCases.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: SettingTableViewCell = tableView.dequeueReusableCell(for: indexPath)
+        cell.setTitle(Setting.allCases[indexPath.item].title)
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+}
---
+++ Projects/App/Sources/Screens/Setting/Views/UserProfileView.swift
@@ -0,0 +1,108 @@
+//
+//  UserProfileView.swift
+//  App
+//
+//  Created by yjiyuni-MN on 2023/01/24.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+import DesignSystem
+import Global
+import UIKit
+
+import SnapKit
+
+class UserProfileView: UIView {
+    
+    // MARK: - UIComponents
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .wkMainPurple
+        return view
+    }()
+    
+    private let containerView = UIView()
+    
+    private var usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .h3B
+        return label
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.font = .b1M
+        label.textColor = .wkSubPurple45
+        return label
+    }()
+    
+    // MARK: - Initalizer
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.setLayout()
+        self.setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    private func setUI() {
+        self.setBackgroundUI()
+        self.setShadowUI()
+    }
+    
+    private func setBackgroundUI() {
+        self.containerView.backgroundColor = UIColor.white
+        self.containerView.makeRounded(radius: 5)
+        self.containerView.layer.borderWidth = 1
+        self.containerView.layer.borderColor = UIColor.wkBlack4.cgColor
+    }
+    
+    private func setShadowUI() {
+        self.containerView.layer.shadowColor = UIColor.wkBlack5.cgColor
+        self.containerView.layer.shadowOpacity = 1
+        self.containerView.layer.shadowRadius = 10
+        self.containerView.layer.masksToBounds = false
+    }
+    
+    private func setLayout() {
+        self.addSubviews([backgroundView, containerView])
+        self.containerView.addSubviews([usernameLabel, emailLabel])
+        
+        self.backgroundView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-40)
+        }
+        
+        self.containerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(94)
+        }
+
+        self.usernameLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(23)
+            make.leading.equalToSuperview().offset(14)
+        }
+
+        self.emailLabel.snp.makeConstraints { make in
+            make.top.equalTo(usernameLabel.snp.bottom).offset(5)
+            make.leading.equalTo(usernameLabel.snp.leading)
+            make.bottom.equalToSuperview().offset(-23)
+        }
+    }
+    
+    func setUsername(text: String) {
+        self.usernameLabel.text = text
+    }
+    
+    func setEmail(text: String) {
+        self.emailLabel.text = text
+    }
+    
+}
