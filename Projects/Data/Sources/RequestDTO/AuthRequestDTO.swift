1907be51f9b856c4b93882a4a0a4749894e58703
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2023-01-25 00:58:44 +0900

    Feat: add example router, service, dto (#68)

---
+++ Projects/Data/Sources/RequestDTO/AuthRequestDTO.swift
@@ -0,0 +1,22 @@
+//
+//  AuthRequestDTO.swift
+//  Data
+//
+//  Created by 김혜수 on 2023/01/23.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+public struct AuthRequestDTO: Encodable {
+    let socialToken: String
+    let nickName: String?
+    
+    public init(socialToken: String, nickName: String?) {
+        self.socialToken = socialToken
+        self.nickName = nickName
+    }
+}
+
+public enum SocialType: String {
+    case APPLE
+    case KAKAO
+}
---
+++ Projects/Data/Sources/ResponseDTO/AuthResponseDTO.swift
@@ -0,0 +1,11 @@
+//
+//  AuthResponseDTO.swift
+//  Data
+//
+//  Created by 김혜수 on 2023/01/23.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+public struct AuthResponseDTO: Decodable {
+    let accessToken: String
+}
