49db2006872a60fdce006aa4c6f0eacbe385874c
dev-madilyn <lollin0@naver.com>
2023-01-26 01:57:48 +0900

    Chore: WriteAbilityEntity 작성 (#60)

---
+++ Projects/App/Sources/Screens/Write/Entity/WriteAbilityEntity.swift
@@ -0,0 +1,14 @@
+//
+//  AbilityEntity.swift
+//  App
+//
+//  Created by madilyn on 2023/01/26.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+import Foundation
+
+struct WriteAbilityEntity {
+    var abilityId: Int = 0
+    var abilityName: String = ""
+}
