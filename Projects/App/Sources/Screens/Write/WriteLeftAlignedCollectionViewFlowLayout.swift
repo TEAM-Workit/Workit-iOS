e29f5aa85eb05967bb1fa09a1ddf849179e80066
dev-madilyn <lollin0@naver.com>
2023-01-29 02:57:22 +0900

    Fix: lint error (#82)

--- Projects/App/Sources/Screens/Write/WriteLeftAlignedCollectionViewFlowLayout.swift
+++ Projects/App/Sources/Screens/Write/WriteLeftAlignedCollectionViewFlowLayout.swift
@@ -16,18 +16,22 @@ final class WriteLeftAlignedCollectionViewFlowLayout: UICollectionViewFlowLayout
         self.minimumInteritemSpacing = 8
         self.sectionInset = .zero
         
-        let attributes = super.layoutAttributesForElements(in: rect)?.map { $0.copy() as! UICollectionViewLayoutAttributes }
+        let attributes: [UICollectionViewLayoutAttributes]? = super.layoutAttributesForElements(in: rect)?.map {
+            $0.copy() as? UICollectionViewLayoutAttributes ?? UICollectionViewLayoutAttributes()
+        } ?? []
         var leftMargin: CGFloat = sectionInset.left
         var maxY: CGFloat = -1.0
+        
         attributes?.forEach { layoutAttribute in
             guard layoutAttribute.representedElementCategory == .cell else { return }
             if layoutAttribute.frame.origin.y >= maxY {
                 leftMargin = 0
             }
             layoutAttribute.frame.origin.x = leftMargin
-            leftMargin += layoutAttribute.frame.width + minimumInteritemSpacing
-            maxY = max(layoutAttribute.frame.maxY , maxY)
+            leftMargin += (layoutAttribute.frame.width) + minimumInteritemSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
         }
+        
         return attributes
     }
 }
