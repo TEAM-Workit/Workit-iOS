1a17dd9e76c7fc5cabcf4d298e8c5aca9f224103
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2023-01-29 20:29:48 +0900

    Feat: create response dto (#86)

---
+++ Projects/Data/Sources/ResponseDTO/UserNicknameResponseDTO.swift
@@ -0,0 +1,11 @@
+//
+//  UserNicknameResponseDTO.swift
+//  Data
+//
+//  Created by 김혜수 on 2023/01/29.
+//  Copyright © 2023 com.workit. All rights reserved.
+//
+
+public struct UserNicknameResponseDTO {
+    let nickname: String
+}
