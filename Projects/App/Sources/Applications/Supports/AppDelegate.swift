aae07a61b473ab2491ffbcf59c3ca1ed63051164
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2023-05-05 22:42:01 +0900

    Feat: fcm 토큰 발급 세팅 (#153)

--- Projects/App/Sources/Applications/Supports/AppDelegate.swift
+++ Projects/App/Sources/Applications/Supports/AppDelegate.swift
@@ -1,11 +1,13 @@
 import UIKit
 
+import FirebaseCore
+import FirebaseMessaging
 import RxKakaoSDKCommon
 import RxKakaoSDKAuth
 import KakaoSDKAuth
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
 
     var window: UIWindow?
 
@@ -14,6 +16,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
         RxKakaoSDK.initSDK(appKey: "41248bd7434ad5d295065a3fc85577ea")
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().isAutoInitEnabled = true
+        Messaging.messaging().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("✅ fcmToken", fcmToken ?? "nil")
+    }
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      Messaging.messaging().apnsToken = deviceToken
     }
 }
