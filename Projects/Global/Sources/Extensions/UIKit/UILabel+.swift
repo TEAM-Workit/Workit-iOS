0b442ffafdb017bfe796c9be9d9c570aed019a32
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2023-05-13 14:26:32 +0900

    Fix: 탈퇴하기 뷰 설명 행간 추가 (#155)

--- Projects/Global/Sources/Extensions/UIKit/UILabel+.swift
+++ Projects/Global/Sources/Extensions/UIKit/UILabel+.swift
@@ -53,21 +53,32 @@ extension UILabel {
     public func indexOfAttributedTextCharacterAtPoint(point: CGPoint) -> Int {
         assert(self.attributedText != nil, "This method is developed for attributed string")
         let textStorage = NSTextStorage(attributedString: self.attributedText!)
-
+        
         let layoutManager = NSLayoutManager()
         textStorage.addLayoutManager(layoutManager)
-
+        
         let textContainer = NSTextContainer(size: self.frame.size)
         textContainer.lineFragmentPadding = 0
         textContainer.maximumNumberOfLines = self.numberOfLines
         textContainer.lineBreakMode = self.lineBreakMode
         layoutManager.addTextContainer(textContainer)
-
+        
         let index = layoutManager.characterIndex(
             for: point,
             in: textContainer,
             fractionOfDistanceBetweenInsertionPoints: nil
         )
         return index
+    }
+    
+    /// 행간 설정
+    public func setLineSpacing(lineSpacing: CGFloat) {
+        if let text = self.text {
+            let attributedStr = NSMutableAttributedString(string: text)
+            let style = NSMutableParagraphStyle()
+            style.lineSpacing = lineSpacing
+            attributedStr.addAttribute(NSAttributedString.Key.paragraphStyle, value: style, range: NSMakeRange(0, attributedStr.length))
+            self.attributedText = attributedStr
+        }
     }
 }
